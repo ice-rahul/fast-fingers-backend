bf196b22dd3bd9aeada61cec6e3ecaf1a3e3a140
ice-rahul <meet4g@gmail.com>
2021-03-09 14:27:47 +0530

    Moving env to deployment

--- connection/dbConnect.js
+++ connection/dbConnect.js
@@ -2,10 +2,10 @@ import mysql from 'mysql';
 
 const connection = mysql.createPool({
   connectionLimit: 20,
-  host: 'ls-e89759f3dbbe271552940eef018fa3b71189b42e.c6xzx62teqsd.ap-southeast-1.rds.amazonaws.com',
-  user: 'dbmasteruser',
-  password: '=~:WfFAi3H3g+bO!lVWZ=ZapVw8%+,9f',
-  database: 'rahul_pesto',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.USER,
 });
 
 // connection.connect();
